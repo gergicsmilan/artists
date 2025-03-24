0c7d1c971b9c718ef261b7e977e52ce696c87063
Gergics Milan <gergics.milan@arukereso.com>
2025-03-24 13:38:01 +0100

    feat - add basic pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { Container } from "@mui/material";
+import { Container, Pagination } from "@mui/material";
 import List from "./components/List/List";
 import { data } from "./mock";
+import { useState } from "react";
 
 const App = () => {
+  const [page, setPage] = useState(1);
+
   return (
     <Container maxWidth="xl">
       <div>search filed</div>
       <List artists={data.data} />
-      <div>Pagination</div>
+      <Pagination
+        count={data.pagination.total_pages}
+        page={page}
+        onChange={(_e, v) => setPage(v)}
+      />
     </Container>
   );
 };
