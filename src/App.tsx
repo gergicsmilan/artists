97b3cad8c139f7a804d5fd370546441952277001
Gergics Milan <gergics.milan@arukereso.com>
2025-03-24 13:58:20 +0100

    feat - use hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,56 @@
-import { Container, Pagination } from "@mui/material";
+import {
+  Container,
+  InputAdornment,
+  Pagination,
+  TextField,
+} from "@mui/material";
 import List from "./components/List/List";
 import { data } from "./mock";
-import { useState } from "react";
+import useURLSearchParams from "./hooks/useURLSearchParams";
+import useArtistsData from "./hooks/useArtistsData";
+import useListActions from "./hooks/useListActions";
+import SearchIcon from "@mui/icons-material/Search";
 
 const App = () => {
-  const [page, setPage] = useState(1);
+  const {
+    setURLSearchParam,
+    getURLSearchParamByKey,
+    getURLSearchParamsString,
+  } = useURLSearchParams();
+
+  const { getArtists } = useArtistsData({
+    getURLSearchParamsString,
+  });
+
+  const { actionState, handleSearch, handlePaginate } = useListActions({
+    getURLSearchParamByKey,
+    setURLSearchParam,
+    getArtists,
+  });
 
   return (
     <Container maxWidth="xl">
-      <div>search filed</div>
+      <TextField
+        sx={{ marginRight: "auto" }}
+        label="Search for artists"
+        size="small"
+        value={actionState.searchTerm}
+        onChange={handleSearch}
+        slotProps={{
+          input: {
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          },
+        }}
+      />
       <List artists={data.data} />
       <Pagination
         count={data.pagination.total_pages}
-        page={page}
-        onChange={(_e, v) => setPage(v)}
+        page={actionState.page}
+        onChange={(_e, v) => handlePaginate(v)}
       />
     </Container>
   );
